f4777ea9c1281803a85578e3f5e0a2f8dbdd1cc5
ersmax <e.masarin@student.vu.nl>
2025-08-30 17:22:39 +0200

    Add Entry to ModelForm

--- learning_logs/forms.py
+++ learning_logs/forms.py
@@ -1,11 +1,22 @@
 from django import forms    # import the module to build forms
 
-from .models import Topic   # import model you need
+from .models import Topic, Entry   # import model you need
 
 class TopicForm(forms.ModelForm):
     """Build a form automatically with ModelForm"""
     class Meta:
         model = Topic
         fields = ['text']       # include only text, not date
-        labels = {'text': ''}
+        labels = {'text': ''}   # blank label for text field
+
+class EntryForm(forms.ModelForm):
+    """Create a form associated with an entry of a topic"""
+    class Meta:
+        model = Entry           # based the class on Entry
+        fields = ['text']       
+        labels = {'text': ''}   # give a blank label to text
+        widgets = {'text':forms.Textarea(attrs={'cols':80})}
+
+
+
 
