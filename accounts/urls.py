9ad19825d03768f02123e09c211bd764651338a8
ersmax <e.masarin@student.vu.nl>
2025-09-06 17:26:27 +0200

    register url path for registration page

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,18 +3,21 @@ Define URL patterns for accounts
    path() build URLS paths, and map URL to views.
    include() enables default authentication URLs provided by Django (eg. login, logout)
 """
-from django.urls import path, include   
+from django.urls import path, include 
+from . import views  
 
 app_name = 'accounts'   # distinguish urls.py of this app accounts from other apps
 urlpatterns = [         # pages that can be reached from localhost/accounts
-    # Include default auth urls.
     
-    # include login and logout.
+    
     # When Django reads URL login page request, localhost:8000/accounts/login, 
     # the word accounts tells Django to look in accounts/urls.py 
-    # and login tells it to send requests to Django's login view
-      
+    # and login tells Django to send requests to Django's default login view
+
+    # Include default auth urls, including login & logout
     path('', include('django.contrib.auth.urls')),  
+    # Registration page
+    path('register/', views.register, name="register"),
 
 ]
 
